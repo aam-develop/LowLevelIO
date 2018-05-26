1f98fe7aa30fcb912c88f5826a19d2e807e2f49a
aam <aam.you@gmail.com>
2018-05-26 20:37:32 +0900

    テスト環境追加

---
+++ LowLevelIO/LowLevelIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowLevelIO
+{
+    /// <summary>
+    /// 低水準インタフェースルーチン
+    /// </summary>
+    class LowLevelIO
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public LowLevelIO()
+        {
+
+        }
+
+
+    }
+}
